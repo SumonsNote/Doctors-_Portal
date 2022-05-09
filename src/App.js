bb5f790c96de1c8105fda390362f19025554d4c2
Sumonsnote <sohankhance@gmail.com>
2022-05-10 00:26:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/Home/About';
+import Appointment from './Pages/Home/Appointment';
+import Reviews from './Pages/Home/Reviews';
+import ContactUs from './Pages/Home/ContactUs';
+import Login from './Pages/Home/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
