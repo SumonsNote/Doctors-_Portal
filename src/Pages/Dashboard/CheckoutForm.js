883cd9329efc8fc731b3fc4319ac56cab10a144c
Sumonsnote <sohankhance@gmail.com>
2022-05-21 01:43:41 +0600

    'almost-ready'

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ appointment }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [transactionId, setTransactionId] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [processing, setProcessing] = useState(false);
+
+    const { _id, price, patient, patientName } = appointment;
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            });
+
+    }, [price])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -24,6 +48,50 @@ const CheckoutForm = () => {
         });
 
         setCardError(error?.message || '')
+        setSuccess('');
+        setProcessing(true)
+
+        const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: patient
+                    },
+                },
+            },
+        );
+
+        if (intentError) {
+            setCardError(intentError?.message);
+            setProcessing(false)
+        }
+        else {
+            setCardError('');
+            setTransactionId(paymentIntent.id);
+            setSuccess('Congrats! Your payment is completed.')
+
+            // store to database
+            const payment = {
+                appointment: _id,
+                transactionId: paymentIntent.id
+            }
+            fetch(`http://localhost:5000/booking/${_id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+            })
+            .then(res => res.json())
+            .then(data => {
+                setProcessing(false)
+                console.log(data);
+            })
+        }
     }
     return (
         <>
@@ -44,12 +112,18 @@ const CheckoutForm = () => {
                         },
                     }}
                 />
-                <button className='btn btn-success btn-sm text-white mt-2' type="submit" disabled={!stripe}>
+                <button className='btn btn-success btn-sm text-white mt-2' type="submit" disabled={!stripe || !clientSecret}>
                     Pay
                 </button>
             </form>
             {
                 cardError && <p className='text-red-500'>{cardError}</p>
+            }
+            {
+                success && <div className='text-green-500'>
+                    <p>{success}  </p>
+                    <p>Your transaction Id: <span className="text-orange-500 font-bold">{transactionId}</span> </p>
+                </div>
             }
         </>
     );
